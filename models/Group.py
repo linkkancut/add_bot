178ea7251207266859236a310a971560222f26de
Shakib Mohammadi <77733647+shakibm83@users.noreply.github.com>
2021-09-15 12:52:37 +0430

    Remove Unused Files

--- models/Group.py
+++
@@ -1,8 +0,0 @@
-class Group:
-    def __init__(self, id, name, selected):
-        self.GroupId = id
-        self.Name = name
-        self.IsSelected = selected
-
-    def toTuple(self):
-        return (self.GroupId, self.Name, self.IsSelected)
--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = [
-    'Group',
-    'Member',
-    'Worker'
-    ]
