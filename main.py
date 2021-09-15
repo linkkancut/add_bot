98de4c4728f0c8fe9239a03bd4280457ac01a1d5
Shakib Mohammadi <77733647+shakibm83@users.noreply.github.com>
2021-09-15 13:26:39 +0430

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from telethon.tl.functions.channels import InviteToChannelRequest
 from telethon.errors import SessionPasswordNeededError
 api_id = "4540261"
 api_hash = "8627c4e0ef04c5cd61afcee91bdaa7de"
-bot_token = "1952174412:AAENqB-CWAkHfJL-iS4dcghTdDnX4eQyHkg"
+bot_token = "1987815947:AAF59zfkpwHPdk01EiI1Mc7ZotEGFvQLPrM"
 
 bot = TelegramClient('bot', api_id, api_hash)
 bot.start(bot_token=bot_token)
